9c3f236b93b0ddda2f9a739e909323cf0e015a27
Teresa Chu <1695478+teresamychu@users.noreply.github.com>
2023-05-10 11:43:05 -0400

    Update remote_state.tf to generic organization name.

--- remote_state.tf
+++ remote_state.tf
@@ -1,39 +1,20 @@
-data "terraform_remote_state" "aws-example-network-dev-us-east-1" {
+data "terraform_remote_state" "aws-example-network" {
   backend = "remote"
   config = {
-    organization = "hcp_waypoint_integration"
+    organization = "{{ .TfcOrgName }}"
     workspaces = {
       name = "aws-example-network"
     }
   }
 }
 
-data "terraform_remote_state" "aws-example-microservice-infra-dev-us-east-1" {
+data "terraform_remote_state" "aws-example-microservice" {
   backend = "remote"
   config = {
-    organization = "hcp_waypoint_integration"
+    organization = "{{ .TfcOrgName }}"
     workspaces = {
       name = "aws-example-microservice-infra"
     }
   }
 }
 
-data "terraform_remote_state" "aws-example-network-prod-us-east-1" {
-  backend = "remote"
-  config = {
-    organization = "hcp_waypoint_integration"
-    workspaces = {
-      name = "aws-example-network"
-    }
-  }
-}
-
-data "terraform_remote_state" "aws-example-microservice-infra-prod-us-east-1" {
-  backend = "remote"
-  config = {
-    organization = "hcp_waypoint_integration"
-    workspaces = {
-      name = "aws-example-microservice-infra"
-    }
-  }
-}
