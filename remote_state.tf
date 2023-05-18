0a966f02e99a2fbd595c76c141d39ceae6fae638
Teresa Chu <teresa.chu@hashicorp.com>
2023-05-18 12:13:12 -0400

    teresa testing org.

--- remote_state.tf
+++ remote_state.tf
@@ -1,15 +1,17 @@
 data "terraform_remote_state" "aws-example-network" {
   backend = "remote"
   config = {
+    organization = "teresa-corp"
     workspaces = {
       name = "aws-example-network"
     }
   }
 }
 
-data "terraform_remote_state" "aws-example-microservice" {
+data "terraform_remote_state" "aws-example-microservice-infra" {
   backend = "remote"
   config = {
+    organization = "teresa-corp"
     workspaces = {
       name = "aws-example-microservice-infra"
     }
